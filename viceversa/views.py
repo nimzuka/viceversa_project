66ca97b3023dd2897f12d789dc53f81831577989
nimzuka <nimzuka@gmail.com>
2021-01-12 11:01:12 +0200

    ViceVersa project complete

--- viceversa/views.py
+++ viceversa/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 def home(request):
 	return render(request, 'home.html')
 
+def reverse(request):
+	user_text = request.GET['usertext']
+	words_number = len(user_text.split())
+	reverse_text = user_text[::-1]
+	return render(request, 'reverse.html', {'usertext': user_text, 'reversetext':reverse_text, 'wordsnumber':words_number})
+
+
+
